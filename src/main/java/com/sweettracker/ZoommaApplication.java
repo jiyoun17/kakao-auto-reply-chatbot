9d84f29a3d99b92080693d40c6b7f41fa6af880e
Anna Kim <annajinee@gmail.com>
2017-07-11 12:33:53 +0900

    first

---
+++ src/main/java/com/sweettracker/ZoommaApplication.java
@@ -0,0 +1,12 @@
+package com.sweettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoommaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoommaApplication.class, args);
+	}
+}
